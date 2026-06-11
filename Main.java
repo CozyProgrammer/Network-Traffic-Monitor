da1179156fc72a66ba9f224b703e34ad2d876bfa
CozyProgrammer <mhasjaved01@gmail.com>
2026-06-11 23:58:11 +0500

    Creating main file

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
-//casually creating the first setup of the whole project
 public class Main{
-public static void main (String [] args){
+    public static void main (String [] args){
 
+        //opening statements
+        System.out.println("Network Traffic Monitor Started");
+        System.out.println("Getting active network connections...");
+
+        //creating object that will read input from the windows
+        ProcessBuilder pb=new ProcessBuilder();
     }
 }
